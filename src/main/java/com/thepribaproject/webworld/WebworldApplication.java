1982bf62b6ffc82f9d7034e8363931a7997e5c27
Tarik Pribisic <pribajaba@gmail.com>
2022-05-10 19:29:46 +0200

    Initial project setup

---
+++ src/main/java/com/thepribaproject/webworld/WebworldApplication.java
@@ -0,0 +1,13 @@
+package com.thepribaproject.webworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thepribaproject/webworld/WebworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thepribaproject.webworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
